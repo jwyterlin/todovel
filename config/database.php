9ebfb929fa81988f68c303c3dd5c8871560ae807
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2014-12-23 11:38:34 -0200

    Comunicação com o banco de dados MySQL configurada.

--- config/database.php
+++ config/database.php
@@ -48,10 +48,10 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST') ?: 'localhost',
-			'database'  => env('DB_DATABASE') ?: 'forge',
-			'username'  => env('DB_USERNAME') ?: 'forge',
-			'password'  => env('DB_PASSWORD') ?: '',
+			'host'      => 'localhost',
+			'database'  => 'todoSchema',
+			'username'  => 'root',
+			'password'  => '123456',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
